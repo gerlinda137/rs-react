a57f60eb3d534e90880b77d8aaa2690a69d6e65e
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-08-07 19:18:51 +0400

    feat: add placeholder for card's poster and fix some styles

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -21,10 +21,16 @@ const Card: React.FC<CardProps> = ({
   Year,
   Type,
 }: CardProps) => {
+  const posterPlaceholder =
+    'https://blocks.astratic.com/img/general-img-portrait.png';
   return (
     <article className={`card`}>
       <button className="card__button">
-        <img src={Poster} alt="Show poster" className="card__image" />
+        <img
+          src={Poster === 'N/A' ? posterPlaceholder : Poster}
+          alt="Show poster"
+          className="card__image"
+        />
         <div className="card__content">
           <h2 className="card__title">{Title}</h2>
           <span className="card__year">{Year}</span>
